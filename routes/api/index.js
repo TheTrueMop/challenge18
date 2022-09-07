a1a62de97a6822aa513a6e8f113466db0fe6fa2c
Dawson W <102449560+TheTrueMop@users.noreply.github.com>
2022-09-07 17:12:30 -0500

    ... finished

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,22 +1,22 @@
 const router = require('express').Router();
 const {
-  getUsers,
-  getSingleUser,
-  createUser,
-  updateUser,
-  deleteUser,
-  addFriend,
-  removeFriend,
-} = require('../../controllers/user-controller');
+  USERS,
+  USER,
+  ADD_USER,
+  EDIT_USER,
+  DELETE_USER,
+  ADD_FRIEND,
+  DELETE_FRIEND,
+} = require('../../controllers/userController');
 
 // /api/users
-router.route('/').get(getUsers).post(createUser);
+router.route('/').get(USERS).post(ADD_USER);
 
 // /api/users/:userId
-router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+router.route('/:userId').get(USER).put(EDIT_USER).delete(DELETE_USER);
 
 // /api/users/:userId/friends/:friendId
-router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+router.route('/:userId/friends/:friendId').post(ADD_FRIEND).delete(DELETE_FRIEND);
 
 module.exports = router;
 
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const db = require('./config/connection');
-const routes = require('./routes');
+const routes = require('../challenge18/routes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
